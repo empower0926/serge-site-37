996e4954bd13c38f9dfc43d5d078d4716fd72112
haaamza <hamzagee@outlook.com>
2022-11-10 17:00:30 +0500

    initial commit

---
+++ precache-manifest.ca7e0640b7777d1d04e313f4d75c4722.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebce7160054d0085894e69a7bcbca099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af064a3439e49dc5415c",
+    "url": "/static/css/2.06fccacf.chunk.css"
+  },
+  {
+    "revision": "54c2149b694bcf9034e2",
+    "url": "/static/css/main.87c37fd5.chunk.css"
+  },
+  {
+    "revision": "af064a3439e49dc5415c",
+    "url": "/static/js/2.f696a210.chunk.js"
+  },
+  {
+    "revision": "54c2149b694bcf9034e2",
+    "url": "/static/js/main.c3a0e6a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c27e92852d23ca14e7382ec5f911f6f8",
+    "url": "/static/media/BG.c27e9285.png"
+  },
+  {
+    "revision": "18ef5b9e132c3de894a1c1bf37afa070",
+    "url": "/static/media/BitmapDashboard.18ef5b9e.png"
+  },
+  {
+    "revision": "3d36e7de48d7de559aaec2ea5f37e8cb",
+    "url": "/static/media/BitmapNewScienceImage.3d36e7de.png"
+  },
+  {
+    "revision": "3f32b0f92a4fb428741f5cdaee1f7bdf",
+    "url": "/static/media/ReadMore2Image.3f32b0f9.png"
+  },
+  {
+    "revision": "e8c4086f1771e8c8a55f1a6682aa3d99",
+    "url": "/static/media/ReadMoreImage.e8c4086f.png"
+  },
+  {
+    "revision": "3d5ad1c0ced4cf271107fed138ac87a3",
+    "url": "/static/media/Video.3d5ad1c0.jpg"
+  },
+  {
+    "revision": "434f57f14fd8d35d3e1765411b3b8327",
+    "url": "/static/media/Wallet.434f57f1.svg"
+  },
+  {
+    "revision": "855d1be90b31ecbcd0469ba3b2071405",
+    "url": "/static/media/bg-mob.855d1be9.png"
+  },
+  {
+    "revision": "622719976182f478159cd5ff7a6aa73d",
+    "url": "/static/media/grey-bg.62271997.jpg"
+  },
+  {
+    "revision": "befa7ef2cea7318e7ef44b075fc305ba",
+    "url": "/static/media/logo_flash.befa7ef2.svg"
+  },
+  {
+    "revision": "2156284dbae58d9015468a3930880dc0",
+    "url": "/static/media/mob-bg-2.2156284d.png"
+  },
+  {
+    "revision": "d2cda0d38ecba38e1000fb50f97e7d8d",
+    "url": "/static/media/mob-bg.d2cda0d3.png"
+  },
+  {
+    "revision": "7f9f479c75333e08bc7c21b238a30f31",
+    "url": "/static/media/purpur-bg.7f9f479c.jpg"
+  }
+]);
